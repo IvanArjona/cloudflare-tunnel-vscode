948ae7bd62693e9d6ba84b46e7a07d57e08a9d8b
IvanArjona <ivanarjonaalonso@gmail.com>
2021-11-22 21:14:09 +0100

    Extract commands

---
+++ src/commands.ts
@@ -0,0 +1,70 @@
+import * as vscode from 'vscode';
+import { CloudflaredClient } from './cloudflared';
+import { showErrorMessage } from './utils';
+
+
+export async function versionCommand(cloudflared: CloudflaredClient) {
+    const message = await cloudflared.version();
+    vscode.window.showInformationMessage(message);
+}
+
+export async function startCommand(cloudflared: CloudflaredClient) {
+    const config = vscode.workspace.getConfiguration('cloudflaretunnel.tunnel');
+    const defaultPort = config.get<number>('defaultPort', 8080);
+    const askForPort = config.get<boolean>('askForPort', true);
+    const hostname = config.get<string>('hostname');
+    let port = defaultPort;
+
+    if (askForPort) {
+        const inputResponse = await vscode.window.showInputBox({
+            title: "Port number",
+            placeHolder: `Select a port. Default: ${defaultPort}`,
+            ignoreFocusOut: true
+        });
+        if (!inputResponse) {
+            return;
+        }
+        port = inputResponse ? parseInt(inputResponse) : defaultPort;
+    }
+
+    try {
+        const tunnelUri = await cloudflared.start(port, hostname);
+        const action = await vscode.window.showInformationMessage(
+            `Your quick Tunnel has been created!\n${tunnelUri}`,
+            'Copy to clipboard',
+            'Open in browser',
+        );
+
+        switch (action) {
+            case 'Copy to clipboard':
+                vscode.env.clipboard.writeText(tunnelUri);
+                break;
+            case 'Open in browser':
+                vscode.env.openExternal(vscode.Uri.parse(tunnelUri));
+                break;
+        }
+    } catch (ex) {
+        showErrorMessage(ex);
+    }
+}
+
+export async function stopCommand(cloudflared: CloudflaredClient) {
+    await cloudflared.stop();
+    const message = 'Cloudflare tunnel stopped';
+    vscode.window.showInformationMessage(message);
+}
+
+export async function isRunningCommand(cloudflared: CloudflaredClient) {
+    const response = await cloudflared.isRunning();
+    const message = `Cloudflare tunnel is${response ? '' : ' not'} running`;
+    vscode.window.showInformationMessage(message);
+}
+
+export async function loginCommand(cloudflared: CloudflaredClient) {
+    try {
+        await cloudflared.login();
+        vscode.window.showInformationMessage('Logged in successfully');
+    } catch (ex) {
+        showErrorMessage(ex);
+    }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 import * as vscode from 'vscode';
 import { CloudflaredClient } from './cloudflared';
 import { CloudflaredDownloader } from './downloader';
-import { showErrorMessage } from './utils';
+import { isRunningCommand, loginCommand, startCommand, stopCommand, versionCommand } from './commands';
 
 let cloudflared: CloudflaredClient;
 
@@ -13,77 +13,19 @@ export async function activate(context: vscode.ExtensionContext) {
 	// Setup Cloudflared client
 	cloudflared = new CloudflaredClient(cloudflaredUri, context);
 
-	const version = vscode.commands.registerCommand('cloudflaretunnel.version', async () => {
-		const message = await cloudflared.version();
-		vscode.window.showInformationMessage(message);
-	});
+	const commands = {
+		version: versionCommand,
+		start: startCommand,
+		isRunning: isRunningCommand,
+		stop: stopCommand,
+		login: loginCommand
+	};
 
-	const start = vscode.commands.registerCommand('cloudflaretunnel.start', async () => {
-		const config = vscode.workspace.getConfiguration('cloudflaretunnel.tunnel');
-		const defaultPort = config.get<number>('defaultPort', 8080);
-		const askForPort = config.get<boolean>('askForPort', true);
-		const hostname = config.get<string>('hostname');
-		let port = defaultPort;
-
-		if (askForPort) {
-			const inputResponse = await vscode.window.showInputBox({
-				title: "Port number",
-				placeHolder: `Select a port. Default: ${defaultPort}`,
-				ignoreFocusOut: true
-			});
-			if (!inputResponse) {
-				return;
-			}
-			port = inputResponse ? parseInt(inputResponse) : defaultPort;
-		}
-
-		try {
-			const tunnelUri = await cloudflared.start(port, hostname);
-			const action = await vscode.window.showInformationMessage(
-				`Your quick Tunnel has been created!\n${tunnelUri}`,
-				'Copy to clipboard',
-				'Open in browser',
-			);
-
-			switch (action) {
-				case 'Copy to clipboard':
-					vscode.env.clipboard.writeText(tunnelUri);
-					break;
-				case 'Open in browser':
-					vscode.env.openExternal(vscode.Uri.parse(tunnelUri));
-					break;
-			}
-		} catch (ex) {
-			showErrorMessage(ex);
-		}
-	});
-
-	const stop = vscode.commands.registerCommand('cloudflaretunnel.stop', async () => {
-		await cloudflared.stop();
-		const message = 'Cloudflare tunnel stopped';
-		vscode.window.showInformationMessage(message);
-	});
-
-	const isRunning = vscode.commands.registerCommand('cloudflaretunnel.isRunning', async () => {
-		const isRunningResponse = await cloudflared.isRunning();
-		const message = `Cloudflare tunnel is${isRunningResponse ? '' : ' not'} running`;
-		vscode.window.showInformationMessage(message);
-	});
-
-	const login = vscode.commands.registerCommand('cloudflaretunnel.login', async () => {
-		try {
-			await cloudflared.login();
-			vscode.window.showInformationMessage('Logged in successfully');
-		} catch (ex) {
-			showErrorMessage(ex);
-		}
-	});
-
-	context.subscriptions.push(version);
-	context.subscriptions.push(start);
-	context.subscriptions.push(isRunning);
-	context.subscriptions.push(stop);
-	context.subscriptions.push(login);
+	for (const [commandName, commandCallback] of Object.entries(commands)) {
+		const callback = async () => commandCallback(cloudflared);
+		const command = vscode.commands.registerCommand(`cloudflaretunnel.${commandName}`, callback);
+		context.subscriptions.push(command);
+	}
 }
 
 // this method is called when your extension is deactivated
